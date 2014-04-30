4afe96ef17d225a4cb853b84225e151092ecd316
Jeff Thomas <jefferyjthomas@gmail.com>
2014-04-30 15:51:00 -0600

    Implemented favoriting

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ActiveRecord::Base
   has_many :posts
   has_many :comments
   has_many :votes, dependent: :destroy
+  has_many :favorites, dependent: :destroy
 
   mount_uploader :avatar, AvatarUploader
 
@@ -12,5 +13,9 @@ class User < ActiveRecord::Base
 
   def role?(base_role)
     role == base_role.to_s
+  end
+
+  def favorited(post)
+    self.favorites.where(post_id: post.id).first
   end
 end
